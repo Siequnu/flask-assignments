a16c899fe4b3fa850e29a12ac678b4e77fb6a85d
Siequnu <11821606+Siequnu@users.noreply.github.com>
2019-10-22 18:20:13 +0800

    Refactor class and lesson code into its own Blueprint

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, SelectMultipleField, DateField, BooleanField, FormField, TextAreaField
+from wtforms import StringField, SubmitField, SelectField, DateField, SelectMultipleField, BooleanField, FormField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired
 from flask_wtf.file import FileField, FileRequired
 from app import db
@@ -21,10 +21,17 @@ class TurmaCreationForm(FlaskForm):
 	turma_label = StringField('Class label', validators=[DataRequired()])
 	turma_term = StringField('Class term', validators=[DataRequired()])
 	turma_year = StringField('Class year', validators=[DataRequired()])
-	class_start_time = StringField('Class start time', validators=[DataRequired()])
-	class_end_time = StringField('Class end time', validators=[DataRequired()])
+	lesson_start_time = StringField('Class start time', validators=[DataRequired()])
+	lesson_end_time = StringField('Class end time', validators=[DataRequired()])
 	edit = SubmitField('Edit class')
 	submit = SubmitField('Create class')
+	
+class LessonForm(FlaskForm):
+	start_time = StringField('Class start time', validators=[DataRequired()])
+	end_time = StringField('Class end time', validators=[DataRequired()])
+	date = DateField('Class date', validators=[DataRequired()])
+	edit = SubmitField('Edit lesson')
+	submit = SubmitField('Create lesson')
 
 		
 class ConfirmationForm (FlaskForm):
